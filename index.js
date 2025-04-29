d48d40206ed4326630e8865d5c64b768e532594e
Talysson Nogueira <110793575+TalyssonN@users.noreply.github.com>
2025-04-29 09:18:45 -0300

    Portfólio

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("re")
+const fs = require('fs');
+
+const arquivoTexto = 'C:/Users/talys/WebstormProjects/Estudos-Front/tarefas.csv'
+
+
+async function buscarArquivo() {
+   
+
+    try {
+        const arquivo = await fs.promises.readFile(arquivoTexto, 'utf-8');
+        console.log(arquivo);
+    }catch (error) {
+        console.error('Erro ao ler o arquivo:', error);
+    }finally{
+        console.log('Operação concluída!')
+    }
+}
+buscarArquivo()
