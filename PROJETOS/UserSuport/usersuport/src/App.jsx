b46cf8c719b1d8baaaaf8cac3fe0437bf0fa510d
Talysson Nogueira <110793575+TalyssonN@users.noreply.github.com>
2025-05-14 11:13:02 -0300

    Front concluido

---
+++ PROJETOS/UserSuport/usersuport/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/login/login';
+import AuthPage from "./components/login/authPage";
+
+function App() {
+  return (
+    <div className="App">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
